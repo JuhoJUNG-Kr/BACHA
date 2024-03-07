e339d8193f9670e0f2fa47c485b24a53529086c6
Juho <jooho293@gmail.com>
2024-03-07 17:15:25 +0100

    Start

---
+++ Bacha/BachaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BachaApp.swift
+//  Bacha
+//
+//  Created by 정주호 on 07/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct BachaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
